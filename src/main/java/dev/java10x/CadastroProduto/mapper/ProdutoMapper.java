8928df478406c716d1a927737ccb0828080b0928
Yut4ka <henriqueyuzo866@gmail.com>
2025-08-24 19:18:16 +0900

    Cria os mapper na produtoMapper

--- src/main/java/dev/java10x/CadastroProduto/mapper/ProdutoMapper.java
+++ src/main/java/dev/java10x/CadastroProduto/mapper/ProdutoMapper.java
@@ -1,7 +1,29 @@
 package dev.java10x.CadastroProduto.mapper;
 
+import dev.java10x.CadastroProduto.dto.ProdutoDto;
+import dev.java10x.CadastroProduto.model.ProdutoModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProdutoMapper {
+
+    private ProdutoDto map(ProdutoModel produtoModel){
+        ProdutoDto produtoDto = new ProdutoDto();
+        produtoDto.setId(produtoModel.getId());
+        produtoDto.setDescricao(produtoModel.getDescricao());
+        produtoDto.setPreco(produtoModel.getPreco());
+        produtoDto.setQuantidade(produtoModel.getQuantidade());
+        produtoDto.setImg_url(produtoModel.getImg_url());
+        return produtoDto;
+    }
+
+    private ProdutoModel map(ProdutoDto produtoDto){
+        ProdutoModel produtoModel = new ProdutoModel();
+        produtoModel.setId(produtoDto.getId());
+        produtoModel.setDescricao(produtoDto.getDescricao());
+        produtoModel.setPreco(produtoDto.getPreco());
+        produtoModel.setQuantidade(produtoDto.getQuantidade());
+        produtoModel.setImg_url(produtoDto.getImg_url());
+        return produtoModel;
+    }
 }
