e2685f895b023b7986b741ebcc9685dfeaa5e92b
Yut4ka <henriqueyuzo866@gmail.com>
2025-08-24 19:10:34 +0900

    Configura produto service

--- src/main/java/dev/java10x/CadastroProduto/controller/ProdutoController.java
+++ src/main/java/dev/java10x/CadastroProduto/controller/ProdutoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("produto")
-public class ProductController {
+public class ProdutoController {
 
 
 }
