fc7ff234dc9e5fb69390210070fb7920fd6c04bf
Yut4ka <henriqueyuzo866@gmail.com>
2025-08-24 19:09:34 +0900

    Configura controller de produto

---
+++ src/main/java/dev/java10x/CadastroProduto/controller/ProductController.java
@@ -0,0 +1,11 @@
+package dev.java10x.CadastroProduto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produto")
+public class ProductController {
+
+
+}
