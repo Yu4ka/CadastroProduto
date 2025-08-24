e425fab9f35d8852404dedd513d4151211449d2c
Yut4ka <henriqueyuzo866@gmail.com>
2025-08-24 18:33:34 +0900

    first commit

---
+++ src/main/java/dev/java10x/CadastroProduto/CadastroProdutoApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroProdutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroProduto/CadastroProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
